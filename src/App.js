a2dd02f3fe311440850d7ac3bf0c9005de4cb723
Shamshuddin Shaik <shamshuddinis726@gmail.com>
2025-12-19 15:37:13 +0530

    All Test Cases Passed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import {Component} from 'react'
-
-import Emoji from './components/Feedback'
+import Feedback from './components/Feedback'
 
 import './App.css'
 
@@ -25,59 +23,6 @@ const resources = {
   loveEmojiUrl: 'https://assets.ccbp.in/frontend/react-js/love-emoji-img.png',
 }
 
-class App extends Component {
-  state = {
-    isEmojiClicked: false,
-  }
-
-  onClickEmoji = () => {
-    this.setState({isEmojiClicked: true})
-  }
-
-  renderFeedbackQsn = () => {
-    const {emojis} = resources
-    return (
-      <div>
-        <h1>How satisfied are you with our customer support performance?</h1>
-        <ul className="emoji-list">
-          {emojis.map(each => (
-            <Emoji
-              resource={each}
-              key={each.id}
-              onClickEmoji={this.onClickEmoji}
-            />
-          ))}
-        </ul>
-      </div>
-    )
-  }
-
-  renderThankYouScreen = () => {
-    const {loveEmojiUrl} = resources
-
-    return (
-      <div className="thank-you-card">
-        <img src={loveEmojiUrl} alt="love emoji" />
-        <h1 className="thank-you-card">Thank You</h1>
-        <p>
-          We will use your feedback to improve our customer support performance.
-        </p>
-      </div>
-    )
-  }
-
-  render() {
-    const {isEmojiClicked} = this.state
-    return (
-      <div>
-        <div className="card">
-          {isEmojiClicked
-            ? this.renderThankYouScreen()
-            : this.renderFeedbackQsn()}
-        </div>
-      </div>
-    )
-  }
-}
+const App = () => <Feedback resources={resources} />
 
 export default App
